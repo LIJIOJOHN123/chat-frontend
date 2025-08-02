649946e9c58aaf00a5439485bf709e899a4d6e80
Lijo John <lijojohnrbs@gmail.com>
2025-08-02 19:22:21 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ChatIcon from './svg/Chat';
+
+const App = () => {
+  return (
+    <>
+    <h1>
+      Welcome Lijo John
+   
+      </h1>
+       <ChatIcon className="red"/>
+      </>
+        
+    );
+}
+ 
+export default App;
